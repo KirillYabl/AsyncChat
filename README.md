57254977359a2ccdff5e3ec11377eb268b1ce04d
KirillYabl <kirill-yablunovskii@mail.ru>
2022-12-31 15:33:28 +0300

    Add new script to send messages

--- README.md
+++ README.md
@@ -11,6 +11,8 @@ pip install -r requirements.txt
 
 ### How to use
 
+#### Chat listener
+
 ```bash
 python chat_listener.py -host HOST -p PORT -hp HISTORY_PATH
 ```
@@ -19,6 +21,18 @@ python chat_listener.py -host HOST -p PORT -hp HISTORY_PATH
   -host HOST host of chat
   -p PORT, --port PORT port of chat
   -hp HISTORY_PATH, --history_path HISTORY_PATH
+
+#### Chat writer
+
+```bash
+python chat_writer.py -host HOST -p PORT -m MESSAGE
+```
+
+Параметры:
+  -host HOST host of chat
+  -p PORT, --port PORT port of chat
+  -m MESSAGE, --message MESSAGE message to send
+
 
 ### References
 
---
+++ chat_writer.py
@@ -0,0 +1,49 @@
+import argparse
+import asyncio
+from dataclasses import dataclass
+
+
+@dataclass
+class Options:
+    host: str
+    port: int
+    message: str
+    token: str = '4632b104-88ff-11ed-8c47-0242ac110002'
+
+
+def make_msg(text: str, is_end: bool = False) -> bytes:
+    """Make suitable for sending message: add newlines and convert to bytes"""
+    text = f'{text}\n'
+    if is_end:
+        # double \n, first for end line and second empty line for end message
+        text += '\n'
+    return text.encode()
+
+
+async def write_to_chat(options: Options) -> None:
+    reader, writer = await asyncio.open_connection(options.host, options.port)
+
+    writer.write(make_msg(options.token))
+    await writer.drain()
+
+    writer.write(make_msg(options.message, True))
+    await writer.drain()
+
+    writer.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog='Async chat writer',
+        description='Script for write to chat message',
+    )
+
+    parser.add_argument('-host', type=str, required=True, help='host of chat')
+    parser.add_argument('-p', '--port', type=int, required=True, help='port of chat')
+    parser.add_argument('-m', '--message', type=str, required=True, help='message to send')
+
+    args = parser.parse_args()
+
+    options = Options(**args.__dict__)
+
+    asyncio.run(write_to_chat(options))
