654371745310f759757a92fc6201f44648f74bf2
KirillYabl <kirill-yablunovskii@mail.ru>
2023-01-13 15:00:33 +0300

    Add message sending by token of user

--- messenger.py
+++ messenger.py
@@ -1,6 +1,7 @@
 import asyncio
 import argparse
 import datetime
+import json
 from dataclasses import dataclass
 import logging
 from pathlib import Path
@@ -15,7 +16,6 @@ logger = logging.getLogger(__name__)
 
 async def read_msgs(host: str, port: int, filepath: Path,
                     messages_queue: asyncio.Queue, messages_to_file_queue: asyncio.Queue) -> None:
-
     # read history of chat
     async with aiofiles.open(filepath, 'r', encoding='UTF8') as f:
         async for message in f:
@@ -40,11 +40,71 @@ async def save_msgs(filepath: Path, queue: asyncio.Queue) -> None:
             await f.write(message)
 
 
+async def send_msgs(host: str, port: int, token: str, queue: asyncio.Queue) -> None:
+    while True:
+        message = await queue.get()
+        await submit_message(host, port, token, message)
+
+
+async def write_message_in_stream(writer: asyncio.StreamWriter, text: str) -> None:
+    text = text.encode()
+    logger.debug(f'SEND: {text}')
+    writer.write(text)
+    await writer.drain()
+
+
+async def authorize_in_chat_by_token(host: str, port: int, token: str) -> bool:
+    """
+    Authorization by token from args
+    Return bool value of authorization result
+    """
+    logger.info(f'authorization...')
+    async with open_connection(host, port) as (reader, writer):
+        greeting_msg = await reader.readline()
+        logger.debug(f'RECEIVE: {greeting_msg.decode().strip()}')
+
+        await write_message_in_stream(writer, f'{token}\n')
+
+        credentials_msg = await reader.readline()
+        logger.debug(f'RECEIVE: {credentials_msg.decode().strip()}')
+
+        if not json.loads(credentials_msg.decode().strip()):
+            logger.error(f'Wrong token {token}')
+            return False
+
+    logger.info(f'success authorization')
+    return True
+
+
+async def submit_message(host: str, port: int, token: str, message: str) -> None:
+    """
+    Submit message in chat.
+    In this function token always in options and always valid
+    """
+    logger.info(f'submit message...')
+    async with open_connection(host, port) as (reader, writer):
+        greeting_msg = await reader.readline()
+        logger.debug(f'RECEIVE: {greeting_msg.decode().strip()}')
+
+        await write_message_in_stream(writer, f'{token}\n')
+
+        authorization_msg = await reader.readline()
+        logger.debug(f'RECEIVE: {authorization_msg.decode().strip()}')
+
+        # double \n because chat require empty string for message sending
+        await write_message_in_stream(writer, f'{message}\n\n')
+
+        logger.info(f'message submitted')
+
+
 @dataclass
 class Options:
     listen_host: str
     listen_port: int
     history_path: Path
+    write_host: str
+    write_port: int
+    token: str
 
 
 if __name__ == '__main__':
@@ -59,6 +119,9 @@ if __name__ == '__main__':
     parser.add_argument('-lp', '--listen_port', type=int, required=True, help='port of chat to listen')
     parser.add_argument('-hp', '--history_path',
                         type=Path, default='chat_history.txt', help='path to file with messages')
+    parser.add_argument('-wh', '--write_host', type=str, required=True, help='host of chat to write')
+    parser.add_argument('-wp', '--write_port', type=int, required=True, help='port of chat to write')
+    parser.add_argument('-t', '--token', type=str, required=True, help='token of registered user')
 
     args = parser.parse_args()
 
@@ -69,8 +132,12 @@ if __name__ == '__main__':
     status_updates_queue = asyncio.Queue()
     messages_to_file_queue = asyncio.Queue()
 
+    is_authorize = authorize_in_chat_by_token(options.write_host, options.write_port, options.token)
+
     loop.run_until_complete(asyncio.gather(
-        read_msgs(options.listen_host, options.listen_port, options.history_path, messages_queue, messages_to_file_queue),
+        read_msgs(options.listen_host, options.listen_port, options.history_path, messages_queue,
+                  messages_to_file_queue),
         save_msgs(options.history_path, messages_to_file_queue),
+        send_msgs(options.write_host, options.write_port, options.token, sending_queue),
         gui.draw(messages_queue, sending_queue, status_updates_queue)
     ))
